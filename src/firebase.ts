45e2c23f5294b204ac37f5170fd255f428708016
Mohd Imran <121624508+ImranliMb0@users.noreply.github.com>
2025-05-19 20:02:15 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBrstxQLRm9R0xhpbeYB_BN3VYYhEF60bo",
-  authDomain: "skillswap-c14c4.firebaseapp.com",
-  projectId: "skillswap-c14c4",
-  storageBucket: "skillswap-c14c4.firebasestorage.app",
-  messagingSenderId: "526578805120",
-  appId: "1:526578805120:web:5197a10e929d0a801c7965",
-  measurementId: "G-R69SP4YJTN"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
